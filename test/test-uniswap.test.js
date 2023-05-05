cdea0f8404218eeb22ab40976f2fb83c525e3001
Rio <onwukachibike@gmail.com>
2023-05-05 18:16:17 +0100

    updated

---
+++ test/test-uniswap.test.js
@@ -0,0 +1,9 @@
+const BN = require("bn.js")
+const IERC20 = artifacts.require("IERC20")
+const TestUniswap = artifacts.require("TestUniswap")
+
+contract("TestUniswap", (accounts) =>{
+    const DAI = "0x6B175474E89094C44Da98b954EedeAC495271d0F"
+    const DAI_WHALE = "0x28C6c06298d514Db089934071355E5743bf21d60"
+    const WBTC = "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599"
+})
