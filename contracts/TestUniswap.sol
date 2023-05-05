129b81d701dd2092dbf1dde678bebc5278422b00
Rio <onwukachibike@gmail.com>
2023-05-05 17:36:27 +0100

    created function swap

--- contracts/TestUniswap.sol
+++ contracts/TestUniswap.sol
@@ -8,3 +8,7 @@ contract TestUniswap {
     address private constant UNISWAP_V2_ROUTER = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
     address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
 }
+
+function swap(
+    address
+)
