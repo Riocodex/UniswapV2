5c9da4bdad8417b90e36573b8f5bcfe7beb95b50
Rio <onwukachibike@gmail.com>
2023-05-05 17:34:39 +0100

    updated contract

---
+++ contracts/TestUniswap.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./interfaces/Uniswap.sol";
+
+contract TestUniswap {
+    address private constant UNISWAP_V2_ROUTER = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+    address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+}
