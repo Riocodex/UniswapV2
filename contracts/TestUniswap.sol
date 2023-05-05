929a175610633ce2f8f8ce4b90b608c6d729a13a
Rio <onwukachibike@gmail.com>
2023-05-05 17:40:36 +0100

    updated swapfunction

--- contracts/TestUniswap.sol
+++ contracts/TestUniswap.sol
@@ -17,6 +17,7 @@ contract TestUniswap {
         address _to
     ) external {
         IERC20(_tokenIn).transferFrom(msg.sender, address(this), _amountIn);
+        IERC20(_tokenIn).approve(UNISWAP_V2_ROUTER, _amountIn);
     }
 
 }
