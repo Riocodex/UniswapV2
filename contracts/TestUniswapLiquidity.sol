c348dd3ef9c63367594fd7bc86845dbd02b30216
Rio <onwukachibike@gmail.com>
2023-05-08 16:07:08 +0100

    created the addliquidity function

--- contracts/TestUniswapLiquidity.sol
+++ contracts/TestUniswapLiquidity.sol
@@ -9,6 +9,10 @@ contract TestUniswapLiquidity {
   address private constant ROUTER = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
   address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
 
-  event
+  event Log(string message, unt256 val);
+
+  function addLiquidity(
+    add
+  )
 
 }
