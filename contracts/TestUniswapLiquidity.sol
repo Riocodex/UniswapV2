f0affe8b6e56f5bcb6add4b53d2a67586b9c53f4
Rio <onwukachibike@gmail.com>
2023-05-08 16:45:07 +0100

    added removeliqidity function

--- contracts/TestUniswapLiquidity.sol
+++ contracts/TestUniswapLiquidity.sol
@@ -45,6 +45,20 @@ contract TestUniswapLiquidity {
     uint256 liquidity = IERC20(pair).balanceOf(address(this));
 
     IERC20(pair).approve(ROUTER,liquidity);
+
+    (uint256 amountA, uint256 amountB) = 
+        IUniswapV2Router(ROUTER).removeLiquidity(
+        _tokenA,
+        _tokenB,
+        liquidity,
+        1,
+        1,
+        address(this),
+        block.timestamp
+        );
+
+        emit Log("amountA", amountA);
+        emit Log("amountB", amountB);
   }
 
 }
