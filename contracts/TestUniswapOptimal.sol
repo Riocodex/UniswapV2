cff5d11fd8042ad50c510237b86dba0c29a915f1
Rio <onwukachibike@gmail.com>
2023-05-09 01:01:03 +0100

    created

---
+++ contracts/TestUniswapOptimal.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./interfaces/Uniswap.sol";
+
+contract TestUniswapOptimal {
+  using SafeMath for uint;
+
+  address private constant FACTORY = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
+  address private constant ROUTER = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+  address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
